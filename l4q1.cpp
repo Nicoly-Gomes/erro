f0baf56946a9234e01e41b321c7d6ecec47ae2c8
Nicoly-Gomes <137960232+Nicoly-Gomes@users.noreply.github.com>
2023-07-02 15:50:15 -0300

    Create l4q1.cpp

---
+++ l4q1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+// Aluna: Nicoly Gomes da Hora, IF23
+//Questão para ler uma string e imprimir ela na tela
+int main() {
+char x[50];
+  cin.get(x,50);
+  cout << x;
+}
